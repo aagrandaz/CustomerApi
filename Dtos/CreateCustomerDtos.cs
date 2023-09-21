0cc44acddfa1891ffcd3d2964f35fb4c4174982e
abelgranda <abelgranda@hotmail.es>
2023-09-20 19:43:37 -0500

    Lower case routes /pi/customer${controller}

--- Dtos/CreateCustomerDtos.cs
+++ Dtos/CreateCustomerDtos.cs
@@ -4,9 +4,11 @@ namespace CustomerApi.Dtos;
 
 public class CreateCustomerDtos
 {
-    [Required(ErrorMessage = "")]
+    [Required(ErrorMessage = "El nombre propio tiene que estar especificado")]
     public string Name { get; set;}
+    [Required(ErrorMessage = "El apellido tiene que estar especificado")]
     public string LastName { get; set; }
+    [RegularExpression("@^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", ErrorMessage = "El email no es correcto")]
     public string Email { get; set;}
     public string Phone { get; set; }
     public string Address { get; set; }
